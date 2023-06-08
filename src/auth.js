c8d1fda78d640d4497fb81dc5dcc5813ed4d99d2
Vincent Siyu Xian <z5480806@ad.unsw.edu.au>
2023-06-08 19:28:27 +1000

    Added adminUserDetails

--- src/auth.js
+++ src/auth.js
@@ -1,20 +1,4 @@
 /**
- * This function authorises the input of the username and password when signing
- * into Toohak. If the email and password match the system, it will allow the 
- * user to sign in. 
- * 
- * @param {string} email 
- * @param {string} password 
- * 
- * @returns {{authUserId: number}}
- */
-function adminAuthLogin(email, password) {
-    return {
-        authUserId: 1
-    }
-}
-
-/**
  * This function registers a new user into Toohak: requires an email password
  * and first & last name to create a valid user, will then generate and return
  * a new userId.
@@ -24,10 +8,54 @@ function adminAuthLogin(email, password) {
  * @param {string} nameFirst
  * @param {string} nameLast
  * 
- * @returns {{authUserId: number}}
+ * @returns {{authUserId: integer}}
  */
 function adminAuthRegister(email, password, nameFirst, nameLast) {
     return {
         authUserId: 1
+    }
+}
+
+/**
+ * This function authorises the input of the username and password when signing
+ * into Toohak. If the email and password match the system, it will allow the 
+ * user to sign in. 
+ * 
+ * @param {string} email 
+ * @param {string} password 
+ * 
+ * @returns {{authUserId: integer}}
+ */
+function adminAuthLogin(email, password) {
+    return {
+        authUserId: 1
+    }
+}
+
+/**
+ * Given an admin user's authUserId, return details about the user.
+ * "name" is the first and last name concatenated with a single space between them
+ * 
+ * @param {integer} authUserId 
+ * 
+ * @returns {{ user:
+ *  { 
+ *      userId: integer,
+ *      email: string,
+ *      numSuccessfulLogins: integer,
+ *      numFailedPasswordsSinceLastLogin: integer
+ *  }
+ * }}
+ */
+function adminUserDetails(authUserId) {
+    return {
+        user:
+        {
+          userId: 1,
+          name: 'Hayden Smith',
+          email: 'hayden.smith@unsw.edu.au',
+          numSuccessfulLogins: 3,
+          numFailedPasswordsSinceLastLogin: 1,
+        }
     }
 }
