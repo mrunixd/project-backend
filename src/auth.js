5c764043fe917197549e50ad6f774b0d6a9a88f1
Vincent Siyu Xian <z5480806@ad.unsw.edu.au>
2023-06-08 19:10:09 +1000

    Added adminAuthRegister (again)

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,31 @@
+/**
+ * This function authorises the input of the username and password when signing
+ * into Toohak. If the email and password match the system, it will allow the 
+ * user to sign in. 
+ * 
+ * @param {string} email 
+ * @param {string} password 
+ * 
+ * @returns {{authUserId: number}}
+ */
+function adminAuthLogin(email, password) {
+    return {
+        authUserId: 1
+    }
+}
+
+/**
+ * This function registers a new user into Toohak: requires an email password
+ * and first & last name to create a valid user, will then generate and return
+ * a new userId.
+ * 
+ * @param {string} email 
+ * @param {string} password 
+ * @param {string} nameFirst
+ * @param {string} nameLast
+ * 
+ * @returns {{authUserId: number}}
+ */
 function adminAuthRegister(email, password, nameFirst, nameLast) {
     return {
         authUserId: 1
