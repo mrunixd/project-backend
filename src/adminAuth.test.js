2a82a5cc063e1f064a07ba20758d60fad0ce17ac
Vincent Siyu Xian <z5480806@ad.unsw.edu.au>
2023-06-16 20:10:24 +1000

    Implemented adminAuthRegister function and edited tests in adminAuth.test.js

--- src/adminAuth.test.js
+++ src/adminAuth.test.js
@@ -1,10 +1,12 @@
 import { adminAuthRegister, adminAuthLogin, adminUserDetails } from "./auth.js";
-import { getData, setData } from "./dataStore.js";
+// import { getData, setData } from "./dataStore.js";
+import { clear } from "./other.js";
 
 //TESTING adminAuthRegister
 describe('Testing adminAuthRegister success', () => {
 
     test('CASE: Successful register', () => {
+        clear();
         let result = adminAuthRegister("vincentxian@gmail.com", "vincentpassword1", "vincent", "xian");
         expect(result).toMatchObject({ authUserId: expect.any(Number) });
     });
@@ -12,43 +14,53 @@ describe('Testing adminAuthRegister success', () => {
 
 describe('Testing adminAuthRegister errors', () => {
     test('CASE: Email address is already in use', () => {
+        clear();
         let result = adminAuthRegister("vincentxian@gmail.com", "vincentpassword1", "vincent", "xian");
-        expect(result).toMatchObject({ error: 'Email address is already in use.' });
+        let result2 = adminAuthRegister("vincentxian@gmail.com", "vincentpassword1", "vincent", "xian");
+        expect(result2).toStrictEqual({ error: 'Email address is already in use' });
     });
 
     test('CASE: Email address is invalid', () => {
+        clear();
         let result = adminAuthRegister("vincent123", "vincentpassword1", "vincent", "xian");
-        expect(result).toMatchObject({ error: 'Email address is not valid.' });
+        expect(result).toStrictEqual({ error: 'Email address is not valid' });
     });
 
     test('CASE: First name is invalid', () => {
+        clear();
         let result = adminAuthRegister("vincentxian@gmail.com", "vincentpassword1", "!!!", "xian");
-        expect(result).toMatchObject({ error: 'First name is invalid' });
+        expect(result).toStrictEqual({ error: 'First name is invalid' });
     });
-
+    
     test('CASE: First name is less than 2 characters or more than 20 characters', () => {
+        clear();
         let result = adminAuthRegister("vincentxian@gmail.com", "vincentpassword1", "v", "xian");
-        expect(result).toMatchObject({ error: 'First name is less than 2 characters or more than 20 characters' });
+        expect(result).toStrictEqual({ error: 'First name is less than 2 characters or more than 20 characters' });
     });
-
+    
     test('CASE: Last name is invalid', () => {
+        clear();
         let result = adminAuthRegister("vincentxian@gmail.com", "vincentpassword1", "vincent", "!!!");
-        expect(result).toMatchObject({ error: 'Last name is invalid' });
+        expect(result).toStrictEqual({ error: 'Last name is invalid' });
+    });
+       
+    test('CASE: Last name is less than 2 characters or more than 20 characters', () => {
+        clear(); 
+        let result = adminAuthRegister("vincentxian@gmail.com", "vincentpassword1", "vincent", "x");
+        expect(result).toStrictEqual({ error: 'Last name is less than 2 characters or more than 20 characters' });
     });
     
-    test('CASE: Last name is less than 2 characters or more than 20 characters', () => {
-        let result = adminAuthRegister("vincentxian@gmail.com", "vincentpassword1", "vincent", "x");
-        expect(result).toMatchObject({ error: 'Last name is less than 2 characters or more than 20 characters' });
+    test('CASE: Password is less than 8 characters', () => {
+        clear();
+        let result = adminAuthRegister("vincentxian@gmail.com", "pass", "vincent", "xian");
+        expect(result).toStrictEqual({ error: 'Password is less than 8 characters' });
     });
 
-    test('CASE: Password is less than 8 characters', () => {
-        let result = adminAuthRegister("vincentxian@gmail.com", "pass", "vincent", "xian");
-        expect(result).toMatchObject({ error: 'Password is less than 8 characters' });
-    });
-    
     test('CASE: Password does not contain at least one number and at least one letter', () => {
+        clear();
+
         let result = adminAuthRegister("vincentxian@gmail.com", "password", "vincent", "xian");
-        expect(result).toMatchObject({ error: 'Password does not contain at least one number and at least one letter' });
+        expect(result).toStrictEqual({ error: 'Password does not contain at least one number and at least one letter' });
     });
 });
 
