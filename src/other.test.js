1541a32f3c53f905cd768242661ed2de14a92b19
Manan Jaiswal <z5481212@ad.unsw.edu.au>
2023-06-22 13:48:59 +1000

    implemented 1 test

--- src/other.test.js
+++ src/other.test.js
@@ -1,6 +1,17 @@
 import { clear } from "./other.js";
+import { adminAuthRegister } from "./auth.js";
+import { adminQuizCreate, adminQuizInfo } from "./quiz.js";
+
 
 test('test clear() returns {}', () => {
     expect(clear()).toStrictEqual({}); 
 });
   
+let person1;
+let quizId;
+test('test clear() using other functions', () => {
+    person1 = adminAuthRegister("manan.j2450@gmail.com", "Abcd1234", "Manan", "Jaiswal");
+    quizId = adminQuizCreate(person1, "COMP1531", "Software Engineering");
+    clear();
+    expect(adminQuizInfo(person1, quizId)).toStrictEqual({ error: expect.any(String)});
+});
