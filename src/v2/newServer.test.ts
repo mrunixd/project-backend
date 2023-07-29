7e361d740ec80673e5e4c717eb4c764bdc5ee989
Vincent Siyu Xian <z5480806@ad.unsw.edu.au>
2023-07-29 22:49:27 +1000

    Added tests for adminQuizSessionStart

--- src/v2/newServer.test.ts
+++ src/v2/newServer.test.ts
@@ -123,7 +123,6 @@ export function requestAdminQuizTransfer(quizId: string, token: string, userEmai
   return response;
 }
 
-// ???
 export function requestAdminQuizQuestion(quizId: string, token: string, questionBody: any) {
   const response = postRequest(`/v2/admin/quiz/${quizId}/question`, { questionBody }, { token });
   return response;
@@ -146,6 +145,11 @@ export function requestAdminQuizQuestionDuplicate(quizId: string, questionId: st
 
 export function requestAdminQuizQuestionDelete(quizId: string, questionId: string, token: string) {
   const response = deleteRequest(`/v2/admin/quiz/${quizId}/question/${questionId}`, {}, { token });
+  return response;
+}
+
+export function requestAdminQuizSessionStart(token: string, quizId: string, autoStartNum: number) {
+  const response = postRequest(`/v1/admin/quiz/${quizId}/session/start`, { autoStartNum }, { token });
   return response;
 }
 
@@ -2796,6 +2800,78 @@ describe('///////Testing /v2/admin/quiz/question/duplicate////////', () => {
       result1 = requestAdminQuizQuestionDuplicate(`${quiz1.body.quizId}`, `${quizQuestion2.body.questionId}`, `${person1.body.token}`);
       expect(result1.body).toStrictEqual({ error: expect.any(String) });
       expect(result1.status).toBe(INPUT_ERROR);
+    });
+  });
+});
+
+describe('/////// TESTING v1/admin/quiz/{quizid}/session/start ///////', () => {
+  beforeEach(() => {
+    person1 = requestAdminAuthRegister('vincentxian@gmail.com', 'password1', 'vincent', 'xian');
+    quiz1 = requestAdminQuizCreate(`${person1.body.token}`, 'first quiz', 'first quiz being tested');
+    quizQuestion1 = requestAdminQuizQuestion(`${quiz1.body.quizId}`, `${person1.body.token}`, quizQuestion1Body);
+  });
+
+  describe('/////// Testing v1/admin/quiz/{quizid}/session/start success', () => {
+    test('CASE: Successful sessionStart with 1 quiz 1 user with autoStartNum 3', () => {
+      result1 = requestAdminQuizSessionStart(`${person1.body.token}`, `${quiz1.body.quizId}`, 3);
+      expect(result1.body).toStrictEqual({ sessionId: result1.body.sessionId });
+      expect(result1.status).toBe(OK);
+    });
+
+    test('CASE: Successful 3rd quiz owned by 2nd user with autoStartNum 10', () => {
+      person2 = requestAdminAuthRegister('manan.j2450@gmail.com', 'Abcd12345', 'Manan', 'Jaiswal');
+      quiz2 = requestAdminQuizCreate(`${person2.body.token}`, 'second quiz', 'second quiz being tested');
+      requestAdminQuizQuestion(`${quiz2.body.quizId}`, `${person2.body.token}`, quizQuestion1Body);
+      quiz3 = requestAdminQuizCreate(`${person2.body.token}`, 'third quiz', 'third quiz being tested');
+      requestAdminQuizQuestion(`${quiz3.body.quizId}`, `${person2.body.token}`, quizQuestion1Body);
+
+      result1 = requestAdminQuizSessionStart(`${person2.body.token}`, `${quiz3.body.quizId}`, 50);
+      expect(result1.body).toStrictEqual({ sessionId: result1.body.sessionId });
+      expect(result1.status).toBe(OK);
+    });
+  });
+  describe('/////// Testing v1/admin/quiz/{quizid}/session/start errors', () => {
+    test('CASE (403): provided token is a valid structure, but is not for a currently logged on session', () => {
+      const sessionId = person1.body.token;
+      result1 = requestAdminQuizSessionStart(`${parseInt(sessionId) - 1}`, `${quiz1.body.quizId}`, 3);
+      expect(result1.body).toStrictEqual({ error: expect.any(String) });
+      expect(result1.status).toStrictEqual(FORBIDDEN);
+    });
+    test('CASE (401): token is not valid structure', () => {
+      result1 = requestAdminQuizSessionStart('hi!!!', `${quiz1.body.quizId}`, 3);
+      expect(result1.body).toStrictEqual({ error: expect.any(String) });
+      expect(result1.status).toStrictEqual(UNAUTHORISED);
+    });
+    test('CASE (401): token is not valid structure', () => {
+      result1 = requestAdminQuizSessionStart('a1aaa', `${quiz1.body.quizId}`, 3);
+      expect(result1.body).toStrictEqual({ error: expect.any(String) });
+      expect(result1.status).toStrictEqual(UNAUTHORISED);
+    });
+    test('CASE (400): Quiz ID does not refer to a valid quiz', () => {
+      result1 = requestAdminQuizSessionStart(`${person1.body.token}`, `${quiz1.body.quizId + 1}`, 3);
+      expect(result1.body).toStrictEqual({ error: expect.any(String) });
+      expect(result1.status).toStrictEqual(INPUT_ERROR);
+    });
+    test('CASE (400): Quiz ID does not refer to a quiz that this user owns', () => {
+      person2 = requestAdminAuthRegister('manan.j2450@gmail.com', 'Abcd12345', 'Manan', 'Jaiswal');
+
+      result1 = requestAdminQuizSessionStart(`${person2.body.token}`, `${quiz1.body.quizId}`, 3);
+      expect(result1.body).toStrictEqual({ error: expect.any(String) });
+      expect(result1.status).toStrictEqual(INPUT_ERROR);
+    });
+    test('CASE (400): autoStartNum is a number greater than 50', () => {
+      result1 = requestAdminQuizSessionStart(`${person1.body.token}`, `${quiz1.body.quizId}`, 51);
+      expect(result1.body).toStrictEqual({ error: expect.any(String) });
+      expect(result1.status).toStrictEqual(INPUT_ERROR);
+    });
+    /// /////////////////////////////////////////////////////////
+    /// /more than 10 sessions not in END state currently exist//
+    /// /////////////////////////////////////////////////////////
+    test('CASE (400): The quiz does not have any questions in it', () => {
+      requestAdminQuizQuestionDelete(`${quiz1.body.quizId}`, `${quizQuestion1.body.questionId}`, `${person1.body.token}`);
+      result1 = requestAdminQuizSessionStart(`${person1.body.token}`, `${quiz1.body.quizId}`, 3);
+      expect(result1.body).toStrictEqual({ error: expect.any(String) });
+      expect(result1.status).toStrictEqual(INPUT_ERROR);
     });
   });
 });
