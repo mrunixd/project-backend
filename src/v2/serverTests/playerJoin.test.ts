37a1615787ee07dc0cc0e519dca369cd150fbcdd
Manan Jaiswal <z5481212@ad.unsw.edu.au>
2023-07-31 16:18:53 +1000

    implemented basic tests for code

---
+++ src/v2/serverTests/playerJoin.test.ts
@@ -0,0 +1,85 @@
+import {
+  requestAdminAuthRegister,
+  requestAdminQuizCreate,
+  deleteRequest,
+  requestAdminQuizQuestion,
+  requestAdminQuizSessionStart,
+  requestPlayerJoin,
+  OK,
+  INPUT_ERROR,
+} from '../helper';
+
+let result1: any;
+let result2: any;
+let person1: any;
+let quiz1: any;
+let quizQuestion1: any;
+let sessionId: any;
+
+beforeEach(() => {
+  deleteRequest('/v1/clear', {});
+  result1 = undefined;
+  result2 = undefined;
+  person1 = undefined;
+  quiz1 = undefined;
+  quizQuestion1 = undefined;
+});
+
+const quizQuestion1Body = {
+  question: 'Who is the Monarch of England?',
+  duration: 4,
+  points: 5,
+  answers: [
+    {
+      answer: 'Prince Charles',
+      correct: false,
+    },
+    {
+      answer: 'King Charles',
+      correct: true,
+    },
+  ],
+  thumbnailUrl:
+    'https://media.sproutsocial.com/uploads/Homepage_Header-Listening.png',
+};
+
+describe('////////TESTING v1/player/join////////', () => {
+  beforeEach(() => {
+    person1 = requestAdminAuthRegister(
+      'manan.j2450@gmail.com',
+      'password1',
+      'Manan ',
+      'Jaiswal'
+    );
+    quiz1 = requestAdminQuizCreate(
+      `${person1.body.token}`,
+      'first quiz',
+      'first quiz being tested'
+    );
+    quizQuestion1 = requestAdminQuizQuestion(
+      `${quiz1.body.quizId}`,
+      `${person1.body.token}`,
+      quizQuestion1Body
+    );
+    sessionId = requestAdminQuizSessionStart(
+      `${person1.body.token}`,
+      `${quiz1.body.quizId}`,
+      3
+    );
+  });
+  describe('TESTING v1/player/join success', () => {
+    test('player joins empty game successfully', () => {
+      result1 = requestPlayerJoin(sessionId, 'Manan Jaiswal');
+      expect(result1.body).toStrictEqual({ playerId: expect.any(Number) });
+      expect(result1.status).toBe(OK);
+    });
+  });
+  describe('TESTING v1/player/join errors', () => {
+    test('CASE 400: player joins game where individual has same name', () => {
+        result1 = requestPlayerJoin(sessionId, 'Manan Jaiswal');
+        result2 = requestPlayerJoin(sessionId, 'Manan Jaiswal');
+        expect(result2.body).toStrictEqual({ error: expect.any(String)});
+        expect(result2.status).toBe(INPUT_ERROR);
+    });
+  });
+});
