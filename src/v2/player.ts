26bbe95250e086d181b8dd264ad9daa6d10e0ca4
Zhi Heng Kevin Zhao <z5476471@ad.unsw.edu.au>
2023-08-02 21:00:01 +1000

    Implemented code for the function playerSendMessage

--- src/v2/player.ts
+++ src/v2/player.ts
@@ -18,7 +18,7 @@ function playerJoin(sessionId: number, name: string): {playerId: number} | Error
 
   const player = { name: name, score: 0, playerId: session.players.length + 1 };
   session.players.push(player);
-  // session.players.push({ name: name, playerId: session.usersRankedByScore.length });
+
   session.players.sort((a, b) => a.name.localeCompare(b.name));
   setData(data);
   return { playerId: session.players.length };
@@ -43,13 +43,14 @@ function createName(): string {
   let numbers = '0123456789';
   let name = '';
 
-  // create  random 5 digit alphabet
+  // Randomises a string of five different alphabet characters.
   for (let i = 0; i < 5; i++) {
     const randomLetterIndex = Math.floor(Math.random() * letters.length);
     name += letters[randomLetterIndex];
     letters = letters.slice(0, randomLetterIndex) + letters.slice(randomLetterIndex + 1);
   }
 
+  // Randomises a string of three different numeric characters.
   for (let i = 0; i < 3; i++) {
     const randomNumberIndex = Math.floor(Math.random() * numbers.length);
     name += numbers[randomNumberIndex];
@@ -58,4 +59,32 @@ function createName(): string {
 
   return name;
 }
-export { playerJoin, playerStatus };
+
+export function playerSendMessage(playerId: number, message: string): Record<string, never> | ErrorObject {
+  const data = getData();
+
+  if (message.length < 2 || message.length > 100) {
+    throw HTTPError(400, { error: 'Message is either less than 1 character or more than 100 characters' });
+  }
+
+  const session = data.sessions.find(session => session.players.some(player => player.playerId === playerId));
+
+  if (!session) {
+    throw HTTPError(400, { error: 'Player ID does not exist' });
+  }
+
+  const player = session.players.find(player => player.playerId === playerId);
+
+  session.messages.push({
+    messageBody: message,
+    playerId: playerId,
+    playerName: player.name,
+    timeSent: Math.floor(Date.now() / 1000)
+  });
+
+  setData(data);
+
+  return {};
+}
+
+export { playerJoin, playerStatus, playerSendMessage };
